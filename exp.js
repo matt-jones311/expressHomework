df7812f23c35985b74e8b12796ca5114e5cc9a11
Matt Jones <mjon8807@yahoo.com>
2018-03-07 21:22:09 -0500

    push test1

--- exp.js
+++ exp.js
@@ -1,8 +1,34 @@
-var express = require ("express") ;
+/*var express = require ("express") ;
 var app = express();
 
 app.get("/",function(req,res){
     res.end("Hello, My World!")
 })
 
-app.listen(8080 || process.env.PORT)
+app.listen(8080 || process.env.PORT);*/
+
+// Pull in required dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Configure the Express application
+var app = express();
+var PORT = process.env.PORT;
+
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
